5f7323bea51d153f82efb135ffcf7dff08652fca
Xiaoxiao <103043833@student.swin.edu.au>
2020-06-03 22:20:19 +1000

    able to dynamically add bookmarks, need css implement

--- src/bookmark.ts
+++ src/bookmark.ts
@@ -1,30 +1,35 @@
-export class Bookmark{
-    Title:string;
-    Icon:string;
-    Favourite:boolean=false
-constructor(pTitle:string,pIcon:string){
-    this.Title=pTitle;
-    this.Icon=pIcon
+export class Bookmark {
+  Title: string;
+  Icon: string;
+  Link: string = "0";
+  Favourite: boolean = false;
+  constructor(pTitle: string, pIcon: string) {
+    this.Title = pTitle;
+    this.Icon = pIcon;
+  }
 }
+export class Bookmarkitem {
+  Bookmark: Bookmark;
+  Item: HTMLElement | null = null;
+  constructor(Pbookmark: Bookmark) {
+    this.Bookmark = Pbookmark;
+  }
+
+  // let row = document.getElementById("row") as HTMLDivElement;
+  // let title = document.getElementById("title") as HTMLElement;
+  // title.innerHTML = this.Bookmark.Title;
+  // let icon = document.getElementById("icon") as HTMLElement;
+  // icon.innerHTML=this.Bookmark.Icon;
+  setUrl(link: string) {
+    this.Bookmark.Link = link;
+  };
 
 
-}
-export class Bookmarkitem{
-    Bookmark:Bookmark;
-    Item:HTMLElement|null=null
-constructor(Pbookmark:Bookmark){
-    this.Bookmark=Pbookmark;
-}
-    setUrl(link:string){
-        let row = document.getElementById("row") as HTMLDivElement;
-        let title = document.getElementById("title") as HTMLElement;
-        title.innerHTML = this.Bookmark.Title;
-        let icon = document.getElementById("icon") as HTMLElement;
-        icon.innerHTML=this.Bookmark.Icon;
-        
-        let url=document.getElementById("url") as HTMLElement;
-
-        url.innerHTML=link;
-        
-    }
+  spawn(Root: HTMLElement) {
+    this.Item = document.createElement("div") as HTMLDivElement;
+    this.Item.appendChild(document.createTextNode(this.Bookmark.Icon));
+    this.Item.appendChild(document.createTextNode(this.Bookmark.Title));
+    this.Item.appendChild(document.createTextNode(this.Bookmark.Link));
+    Root.appendChild(this.Item);
+  };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Bookmark, Bookmarkitem } from "./bookmark";
 let add=document.getElementById("add") as HTMLButtonElement;
-add.onclick=function(){
 let bookmark1=new Bookmark("w3school","icon");
 let bookmarkitem1=new Bookmarkitem(bookmark1);
+let row=document.getElementById("row") as HTMLDivElement;
+add.onclick=function(){
 bookmarkitem1.setUrl("http://item1");
+bookmarkitem1.spawn(row);
+
 }
 
